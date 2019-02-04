6a85c0b0477bc2eb194a9f1389ae3cf785b748de
Hao-Han Pan <c.avantin94@gmail.com>
2019-02-04 16:06:52 -0600

    Create basic console version of code

---
+++ src/console.js
@@ -0,0 +1,85 @@
+const Solver = function() {
+  this.field = [];
+};
+
+Solver.prototype.solveField = function(newField) {
+  this.field = newField;
+  this.scanField();
+  this.printSolution();
+  return this.field;
+};
+
+Solver.prototype.printSolution = function() {
+  console.log(
+    field.map((row) => {
+      return row.join(' ');
+    }).join('\n')
+  );
+}
+
+Solver.prototype.scanField = function() {
+  for (var i = 0; i < this.field.length; i++) {
+    for (var j = 0; j < this.field[i].length; j++) {
+      this.solveBlock(j, i);
+    }
+  }
+};
+
+Solver.prototype.solveBlock = function(x, y) {
+  const value = this.field[y][x];
+  if (typeof value === 'number') {
+    if (value === 0) {
+      this.markSurroundingBlanks(x, y, 'C');
+    } else {
+      const mines = this.countSurrounding(x, y, 'M');
+      const blanks = this.countSurrounding(x, y, '?');
+      if (value === mines) {
+        this.markSurroundingBlanks(x, y, 'C');
+      } else if (value === blanks + mines) {
+        this.markSurroundingBlanks(x, y, 'M');
+      }
+    }
+  }
+};
+
+Solver.prototype.countSurrounding = function(x, y, val) {
+  let count = 0;
+  for (var i = -1; i < 2; i++) {
+    for (var j = -1; j < 2; j++) {
+      const adjX = x + i;
+      const adjY = y + j;
+      if ((i !== adjX || j !== adjY) && isInBounds(adjX, this.field[0].length) && isInBounds(adjY, this.field.length)) {
+        if (this.field[adjY][adjX] === val) {
+          count++;
+        }
+      }
+    }
+  }
+  return count;
+};
+
+Solver.prototype.markSurroundingBlanks = function(x, y, val) {
+  for (var i = -1; i < 2; i++) {
+    for (var j = -1; j < 2; j++) {
+      const adjX = x + i;
+      const adjY = y + j;
+      if ((i !== adjX || j !== adjY) && isInBounds(adjX, this.field[0].length) && isInBounds(adjY, this.field.length)) {
+        if (this.field[adjY][adjX] === '?') {
+          this.field[adjY][adjX] = val;
+        }
+      }
+    }
+  }
+};
+
+Solver.prototype.markMine = function(x, y) {
+  this.field[y][x] = 'M';
+};
+
+Solver.prototype.markClear = function(x, y) {
+  this.field[y][x] = 'C';
+};
+
+const isInBounds = function(i, max) {
+  return (i > -1 && i < max);
+}
