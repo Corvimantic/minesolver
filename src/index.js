2febd0d0e25d6e16adf26f3b1bda1bd8e996b227
Hao-Han Pan <c.avantin94@gmail.com>
2019-02-05 18:06:29 -0600

    Add error handling stubs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ const solver = new Solver;
 $('#solve').click((e) => {
   const input = $('#input').text();
   const field = convertToArray(input);
-  const solution = solver.solveField(field);
-  console.log(solution);
+  if (isValidArray(field)) {
+    const solution = solver.solveField(field);
+    displaySolution(solution);
+  } else {
+    displayError();
+  }
 });
+
+const displaySolution = function(solution) {
+  const field = convertToText(solution);
+  $('#solution-area').show();
+  $('#solution').text(field);
+};
+
+const displayError = function() {
+};
